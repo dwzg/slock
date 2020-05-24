deedfdc64a7e912af3f302154e30bf95d355b28f
Dennis Witzig <dennis@wtzg.de>
2020-05-24 14:42:23 +0200

    Add personal config file

---
+++ config.h
@@ -0,0 +1,25 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT]   = "black",     /* after initialization */
+	[INPUT]  = "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS]   = "#007755",       /* CapsLock on */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
+
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
